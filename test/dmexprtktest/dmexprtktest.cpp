238be83ecb65c778e98df4bfdee574cf58e3cf35
brinkqiang <brink.qiang@gmail.com>
2025-05-05 10:28:46 +0800

    add

--- test/dmexprtktest/dmexprtktest.cpp
+++ test/dmexprtktest/dmexprtktest.cpp
@@ -1,27 +1,37 @@
+#include <iostream>
+#include <exprtk.hpp>
+#include <vector>
 
-#include "exprtk.hpp"
-#include "dmformat.h"
+// 定义符号表和表达式解析器
+typedef exprtk::symbol_table<double> symbol_table_t;
+typedef exprtk::expression<double> expression_t;
+typedef exprtk::parser<double> parser_t;
 
-int main()
-{
-    double x = 1.618;
-    double y = 2.618;
+int main() {
+    // 创建符号表
+    symbol_table_t symbol_table;
 
-    exprtk::symbol_table<double> symbol_table;
-    symbol_table.add_constants();
+    // 定义变量
+    double x = 0;
+    double y = 0;
     symbol_table.add_variable("x", x);
     symbol_table.add_variable("y", y);
 
-    exprtk::parser<double> parser;
+    // 将符号表添加到表达式中
+    expression_t expr;
+    expr.register_symbol_table(symbol_table);
+    expr.register_symbol_table(symbol_table);
 
-    exprtk::expression<double> expression;
-    expression.register_symbol_table(symbol_table);
-    std::string strExp = "if((x > y), x^2, y^2)";
-    if (!parser.compile(strExp, expression))
+    // 解析表达式
+    parser_t parser;
+    if (!parser.compile("x + y - 10", expr))
     {
-        fmt::print("Parser Error: {}\tExpression: {}\n", parser.error(), strExp);
         return -1;
     }
-    fmt::print("Parser Result: {}\tExpression: {}\n", expression.value(), strExp);
+
+    // 输出结果
+    auto results = expr.value();
+
+
     return 0;
 }
